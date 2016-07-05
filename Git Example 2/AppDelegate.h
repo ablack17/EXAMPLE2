3f68c3407a71f02cd40c86a47b542cfcf6c4ad70
Aislin Black <ablack17@newarka.edu>
2016-07-05 14:27:27 -0400

    Initial Commit

---
+++ Git Example 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Example 2
+//
+//  Created by Aislin Philomena Black on 7/5/16.
+//  Copyright © 2016 Aislin Philomena Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Example 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Example 2
+//
+//  Created by Aislin Philomena Black on 7/5/16.
+//  Copyright © 2016 Aislin Philomena Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
